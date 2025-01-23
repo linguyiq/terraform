928531a1efd99513ee0adfb03170dbee8c79f40e
linguyiq <masabaghce@hotmail.com>
2025-01-23 13:10:21 -0600

    made variables changes

--- aws-vpc-subnets/main.tf
+++ aws-vpc-subnets/main.tf
@@ -5,16 +5,16 @@ provider "aws" {
 resource "aws_vpc" "dev-vpc" {
   cidr_block = var.vpc_cidr_block
   tags =  {
-    Name = "Development"
+    Name = var.env_prefix
   }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.dev-vpc.id
   cidr_block = var.subnet_1_cidr_block
-  availability_zone = "us-east-1d"
+  availability_zone = var.az_subnet_1
   tags =  {
-    Name = "subnet-dev-1"
+    Name = "${var.env_prefix}-subnet-1"
   }
 }
 
@@ -25,33 +25,33 @@ data "aws_vpc" "existing-vpc"{
 resource "aws_subnet" "dev-subnet-2" {
   vpc_id = data.aws_vpc.existing-vpc.id
   cidr_block = var.subnet_2_cidr_block
-  availability_zone = "us-east-1e"
+  availability_zone = var.az_subnet_2
   tags =  {
-    Name = "subnet-dev-2"
+    Name = "${var.env_prefix}-subnet-2"
   }
 }
 
 resource "aws_subnet" "dev-subnet-3" {
   vpc_id = data.aws_vpc.existing-vpc.id
   cidr_block = var.subnet_3_cidr_block
-  availability_zone = "us-east-1f"
+  availability_zone = var.az_subnet_3
   tags =  {
-      Name = "subnet-dev-3"
+      Name = "${var.env_prefix}-subnet-3"
   }
 }
 
-output "dev-vpc-id" {
-  value = aws_vpc.dev-vpc.id
-}
+# output "${var.env_prefix}-vpc-id" {
+#   value = aws_vpc.dev-vpc.id
+# }
 
-output "dev-subnet-1-id" {
-  value = aws_subnet.dev-subnet-1.id
-}
+# output "${var.env_prefix}-subnet-1-id" {
+#   value = aws_subnet.dev-subnet-1.id
+# }
 
-output "dev-subnet-2-id" {
-  value = aws_subnet.dev-subnet-2.id
-}
+# output "${var.env_prefix}-subnet-2-id" {
+#   value = aws_subnet.dev-subnet-2.id
+# }
 
-output "dev-subnet-3-id" {
-  value = aws_subnet.dev-subnet-3.id
-}
+# output "${var.env_prefix}-subnet-3-id" {
+#   value = aws_subnet.dev-subnet-3.id
+# }
