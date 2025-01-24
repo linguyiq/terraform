78ce6f041983aa101d19660b256fd0befaea830e
linguyiq <masabaghce@hotmail.com>
2025-01-23 18:59:44 -0600

    adding rt & sg to vpc

--- aws-vpc-subnets/main.tf
+++ aws-vpc-subnets/main.tf
@@ -9,6 +9,50 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
+  tags =  {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+# resource "aws_route_table" "dev-rtb" {
+#   vpc_id = aws_vpc.dev-vpc.id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.dev-igw.id
+#   }
+#   tags =  {
+#     Name = "${var.env_prefix}-rtb"
+#   }
+# }
+
+resource "aws_default_route_table" "df-rtb" {
+  default_route_table_id = aws_vpc.dev-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-igw.id
+  }
+  tags =  {
+    Name = "${var.env_prefix}-df-rtb"
+  }
+}
+
+resource "aws_route_table_association" "dev-rtb-subnet-1" {
+  subnet_id = aws_subnet.dev-subnet-1.id
+  route_table_id = aws_default_route_table.df-rtb.id
+}
+
+resource "aws_route_table_association" "dev-rtb-subnet-2" {
+  subnet_id = aws_subnet.dev-subnet-2.id
+  route_table_id = aws_default_route_table.df-rtb.id
+}
+
+resource "aws_route_table_association" "dev-rtb-subnet-3" {
+  subnet_id = aws_subnet.dev-subnet-3.id
+  route_table_id = aws_default_route_table.df-rtb.id
+}
+
 resource "aws_subnet" "dev-subnet-1" {
   vpc_id = aws_vpc.dev-vpc.id
   cidr_block = var.subnet_1_cidr_block
@@ -16,14 +60,14 @@ resource "aws_subnet" "dev-subnet-1" {
   tags =  {
     Name = "${var.env_prefix}-subnet-1"
   }
-}
+} 
 
-data "aws_vpc" "existing-vpc"{
-  cidr_block = var.vpc_cidr_block
-}
+# data "aws_vpc" "existing-vpc" {
+#   cidr_block = var.vpc_cidr_block
+# }
 
 resource "aws_subnet" "dev-subnet-2" {
-  vpc_id = data.aws_vpc.existing-vpc.id
+  vpc_id = aws_vpc.dev-vpc.id
   cidr_block = var.subnet_2_cidr_block
   availability_zone = var.az_subnet_2
   tags =  {
@@ -32,7 +76,7 @@ resource "aws_subnet" "dev-subnet-2" {
 }
 
 resource "aws_subnet" "dev-subnet-3" {
-  vpc_id = data.aws_vpc.existing-vpc.id
+  vpc_id = aws_vpc.dev-vpc.id
   cidr_block = var.subnet_3_cidr_block
   availability_zone = var.az_subnet_3
   tags =  {
@@ -40,6 +84,35 @@ resource "aws_subnet" "dev-subnet-3" {
   }
 }
 
+resource "aws_default_security_group" "df-sg" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags =  {
+      Name = "${var.env_prefix}-df-sg"
+  }
+}
 # output "${var.env_prefix}-vpc-id" {
 #   value = aws_vpc.dev-vpc.id
 # }
