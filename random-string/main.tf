a235c1a80504f46c3db50f566db4718ae11776d4
linguyiq <masabaghce@hotmail.com>
2025-06-12 10:53:17 -0500

    terraform fmt

--- random-string/main.tf
+++ random-string/main.tf
@@ -1,10 +1,10 @@
 resource "random_string" "my_random" {
-    length  = 16
-    special = false
-    upper   = false
-    lower   = true
-    numeric = true
-    keepers = {
-        always = timestamp()
-    }
+  length  = 16
+  special = false
+  upper   = false
+  lower   = true
+  numeric = true
+  keepers = {
+    always = timestamp()
+  }
 }
